edf0d8b76185a1bf80ac2bcf0078bab3e8facb03
om3n <dddd04390@gmail.com>
2026-04-09 09:56:22 +0400

    Refactor doctor management and menu options
    Updated doctor ID assignment and modified menu options. Removed getDoctorById function and adjusted doctor initialization.

--- layihe.cpp
+++ layihe.cpp
@@ -1,4 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <conio.h>
 using namespace std;
@@ -34,7 +34,8 @@ void addDoctor() {
 
     Doctor* d = new Doctor;
 
-    cout << "ID: "; cin >> d->id;
+    d->id = countDoctors + 1; // 🔥 AUTO ID
+
     cout << "Ad: "; cin >> d->ad;
     cout << "Soyad: "; cin >> d->soyad;
     cout << "Ixtisas: "; cin >> d->ixtisas;
@@ -44,7 +45,7 @@ void addDoctor() {
 
     addToArray(d);
 
-    cout << "\nElave olundu!\n";
+    cout << "\nElave olundu! ID: " << d->id << endl;
     system("pause");
 }
 
@@ -57,19 +58,14 @@ void updateDoctor() {
         return;
     }
 
-    const char* menu[2] = {
-        "By ID",
-        "Siyahidan sec"
-    };
-
-    int sec = 0;
+    int index = 0;
 
     while (true) {
         system("cls");
 
-        for (int i = 0; i < 2; i++) {
-            cout << menu[i];
-            if (i == sec) cout << " <-";
+        for (int i = 0; i < countDoctors; i++) {
+            cout << doctors[i]->ad << " " << doctors[i]->soyad;
+            if (i == index) cout << " <-";
             cout << endl;
         }
 
@@ -78,73 +74,20 @@ void updateDoctor() {
         if (key == 224 || key == 0) {
             key = _getch();
 
-            if (key == 72) {
-                sec--;
-                if (sec < 0) sec = 1;
-            }
-            else if (key == 80) {
-                sec++;
-                if (sec > 1) sec = 0;
-            }
+            if (key == 72) index = (index - 1 + countDoctors) % countDoctors;
+            else if (key == 80) index = (index + 1) % countDoctors;
         }
-        else if (key == 13) break;
-    }
+        else if (key == 13) {
+            cout << "Yeni Ad: "; cin >> doctors[index]->ad;
+            cout << "Yeni Soyad: "; cin >> doctors[index]->soyad;
+            cout << "Yeni Ixtisas: "; cin >> doctors[index]->ixtisas;
+            cout << "Yeni Tecrube: "; cin >> doctors[index]->tecrube;
+            cout << "Yeni Nomre: "; cin >> doctors[index]->nomre;
+            cout << "Yeni Mail: "; cin >> doctors[index]->mail;
 
-    if (sec == 0) {
-        int id;
-        cout << "ID daxil et: ";
-        cin >> id;
-
-        for (int i = 0; i < countDoctors; i++) {
-            if (doctors[i]->id == id) {
-                cout << "Yeni Ad: "; cin >> doctors[i]->ad;
-                cout << "Yeni Soyad: "; cin >> doctors[i]->soyad;
-                cout << "Yeni Ixtisas: "; cin >> doctors[i]->ixtisas;
-                cout << "Yeni Tecrube: "; cin >> doctors[i]->tecrube;
-                cout << "Yeni Nomre: "; cin >> doctors[i]->nomre;
-                cout << "Yeni Mail: "; cin >> doctors[i]->mail;
-
-                cout << "\nUpdate olundu!\n";
-                system("pause");
-                return;
-            }
-        }
-
-        cout << "Tapilmadi!\n";
-        system("pause");
-    }
-    else {
-        int index = 0;
-
-        while (true) {
-            system("cls");
-
-            for (int i = 0; i < countDoctors; i++) {
-                cout << doctors[i]->ad << " " << doctors[i]->soyad;
-                if (i == index) cout << " <-";
-                cout << endl;
-            }
-
-            int key = _getch();
-
-            if (key == 224 || key == 0) {
-                key = _getch();
-
-                if (key == 72) index = (index - 1 + countDoctors) % countDoctors;
-                else if (key == 80) index = (index + 1) % countDoctors;
-            }
-            else if (key == 13) {
-                cout << "Yeni Ad: "; cin >> doctors[index]->ad;
-                cout << "Yeni Soyad: "; cin >> doctors[index]->soyad;
-                cout << "Yeni Ixtisas: "; cin >> doctors[index]->ixtisas;
-                cout << "Yeni Tecrube: "; cin >> doctors[index]->tecrube;
-                cout << "Yeni Nomre: "; cin >> doctors[index]->nomre;
-                cout << "Yeni Mail: "; cin >> doctors[index]->mail;
-
-                cout << "\nUpdate olundu!\n";
-                system("pause");
-                return;
-            }
+            cout << "\nUpdate olundu!\n";
+            system("pause");
+            return;
         }
     }
 }
@@ -235,7 +178,6 @@ void getDoctors() {
             Doctor* d = doctors[indexes[secDoc]];
 
             system("cls");
-            cout << "ID: " << d->id << endl;
             cout << "Ad: " << d->ad << endl;
             cout << "Soyad: " << d->soyad << endl;
             cout << "Ixtisas: " << d->ixtisas << endl;
@@ -249,84 +191,38 @@ void getDoctors() {
     }
 }
 
-void getDoctorById() {
-    system("cls");
-
-    int id;
-    cout << "ID daxil et: ";
-    cin >> id;
-
-    for (int i = 0; i < countDoctors; i++) {
-        if (doctors[i]->id == id) {
-            cout << "\nID: " << doctors[i]->id << endl;
-            cout << "Ad: " << doctors[i]->ad << endl;
-            cout << "Soyad: " << doctors[i]->soyad << endl;
-            cout << "Ixtisas: " << doctors[i]->ixtisas << endl;
-            cout << "Tecrube: " << doctors[i]->tecrube << endl;
-            cout << "Nomre: " << doctors[i]->nomre << endl;
-            cout << "Mail: " << doctors[i]->mail << endl;
-
-            system("pause");
-            return;
-        }
-    }
-
-    cout << "Tapilmadi!\n";
-    system("pause");
-}
-
 void showMenu(int secim) {
     system("cls");
 
-    const char* menu[5] = {
+    const char* menu[4] = {
         "Add Doctor",
         "Update Doctor",
         "Get Doctors",
-        "Get Doctor By ID",
         "Exit"
     };
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 4; i++) {
         cout << menu[i];
-
-        if (i == secim)
-            cout << " <-";
-
+        if (i == secim) cout << " <-";
         cout << endl;
     }
 }
 
 void initDoctors() {
-    Doctor* d1 = new Doctor{ 1, "Ali", "Huseynov", "Kardio", 10, "0501111111", "ali@mail.com" };
-    addToArray(d1);
-
-    Doctor* d2 = new Doctor{ 2, "Aysel", "Memmedova", "Kardio", 7, "0502222222", "aysel@mail.com" };
-    addToArray(d2);
-
-    Doctor* d3 = new Doctor{ 3, "Rauf", "Aliyev", "Kardio", 12, "0503333333", "rauf@mail.com" };
-    addToArray(d3);
-
-    Doctor* d4 = new Doctor{ 4, "Nigar", "Quliyeva", "Nevroloq", 8, "0504444444", "nigar@mail.com" };
-    addToArray(d4);
-
-    Doctor* d5 = new Doctor{ 5, "Elvin", "Mammadli", "Nevroloq", 6, "0505555555", "elvin@mail.com" };
-    addToArray(d5);
-
-    Doctor* d6 = new Doctor{ 6, "Leyla", "Hesenova", "Nevroloq", 9, "0506666666", "leyla@mail.com" };
-    addToArray(d6);
-
-    Doctor* d7 = new Doctor{ 7, "Orxan", "Baxshaliyev", "Cerrah", 11, "0507777777", "orxan@mail.com" };
-    addToArray(d7);
-
-    Doctor* d8 = new Doctor{ 8, "Sevda", "Ibrahimova", "Cerrah", 5, "0508888888", "sevda@mail.com" };
-    addToArray(d8);
-
-    Doctor* d9 = new Doctor{ 9, "Tural", "Suleymanov", "Cerrah", 13, "0509999999", "tural@mail.com" };
-    addToArray(d9);
+    addToArray(new Doctor{ 1,"Ali","Huseynov","Kardio",10,"0501111111","ali@mail.com" });
+    addToArray(new Doctor{ 2,"Aysel","Memmedova","Kardio",7,"0502222222","aysel@mail.com" });
+    addToArray(new Doctor{ 3,"Rauf","Aliyev","Kardio",12,"0503333333","rauf@mail.com" });
+    addToArray(new Doctor{ 4,"Nigar","Quliyeva","Nevroloq",8,"0504444444","nigar@mail.com" });
+    addToArray(new Doctor{ 5,"Elvin","Mammadli","Nevroloq",6,"0505555555","elvin@mail.com" });
+    addToArray(new Doctor{ 6,"Leyla","Hesenova","Nevroloq",9,"0506666666","leyla@mail.com" });
+    addToArray(new Doctor{ 7,"Orxan","Baxshaliyev","Cerrah",11,"0507777777","orxan@mail.com" });
+    addToArray(new Doctor{ 8,"Sevda","Ibrahimova","Cerrah",5,"0508888888","sevda@mail.com" });
+    addToArray(new Doctor{ 9,"Tural","Suleymanov","Cerrah",13,"0509999999","tural@mail.com" });
 }
 
 int main() {
     initDoctors();
+
     int secim = 0;
 
     while (true) {
@@ -339,11 +235,11 @@ int main() {
 
             if (key == 72) {
                 secim--;
-                if (secim < 0) secim = 4;
+                if (secim < 0) secim = 3;
             }
             else if (key == 80) {
                 secim++;
-                if (secim > 4) secim = 0;
+                if (secim > 3) secim = 0;
             }
         }
         else if (key == 13) {
@@ -351,8 +247,7 @@ int main() {
             case 0: addDoctor(); break;
             case 1: updateDoctor(); break;
             case 2: getDoctors(); break;
-            case 3: getDoctorById(); break;
-            case 4: return 0;
+            case 3: return 0;
             }
         }
     }
